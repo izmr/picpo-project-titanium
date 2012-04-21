2614a627f8ada3af7f9c0375a384a2e2099064b4
marimofire <marimofire@gmail.com>
2012-04-21 19:33:52 +0900

    add app/model/config.js

---
+++ Resources/app/model/config.js
@@ -0,0 +1,10 @@
+tt.model.config = {};
+
+(function(){
+	tt.model.config.itemSearch = {
+		baseUrl: 'http://api.rakuten.co.jp/rws/3.0/rest?',
+		operation: 'ItemSearch',
+		version: '2010-09-15',
+		developerId: '24e3e9e429fc555ba72188fc4f79ed80'
+	};
+})();
